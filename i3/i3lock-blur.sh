ea9dd325202898f62f4b4345b931696ad066b467
gjuanillo <gjuanillo.dev@gmail.com>
2025-06-19 18:12:36 +0800

    Configure i3lock to Blur Screen When Locked

---
+++ i3/i3lock-blur.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define temp files
+IMAGE=/tmp/screen_locked.png
+BLURRED=/tmp/screen_locked_blur.png
+
+# Take a screenshot of the full desktop (both monitors)
+scrot "$IMAGE"
+
+# Blur the screenshot using imagemagick
+convert "$IMAGE" -blur 0x8 "$BLURRED"
+
+# Lock the screen with the blurred image
+i3lock -i "$BLURRED" --nofork
